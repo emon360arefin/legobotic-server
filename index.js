e561fd5a7abc1769b82af8243ebac192f0dab9aa
Emon Arefin <emon360degree@gmail.com>
2023-05-19 20:37:33 +0600

    POST added for Add toy

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mongodb1.tgvctz1.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,22 +30,39 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
+        // client.connect();
 
-        const database = client.db("toysDB");
-        const toysCollection = database.collection("toys");
+        const toysCollection = client.db("toysDB").collection("toys");
+        // const sellersCollection = client.db("toysDB").collection("sellertoys");
 
         app.get('/toys', async (req, res) => {
             const cursor = toysCollection.find();
             const result = await cursor.toArray();
             res.send(result)
         })
+
+        app.get('/toy/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await toysCollection.findOne(query);
+            res.send(result)
+        })
+
+        app.post('/toys', async (req, res) => {
+            const toyForm = req.body;
+            console.log(toyForm);
+            const result = await toysCollection.insertOne(toyForm);
+            res.send(result);
+        })
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        // await client.close();
+        // client.close();
     }
 }
 run().catch(console.dir);
