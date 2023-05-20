8e890748f0c7169fd32e563f60a49098e2940bd5
Emon Arefin <emon360degree@gmail.com>
2023-05-20 23:59:48 +0600

    DeleteOne Added

--- index.js
+++ index.js
@@ -65,6 +65,13 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/toy/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await toysCollection.deleteOne(query);
+            res.send(result)
+        })
+
 
 
         // Send a ping to confirm a successful connection
